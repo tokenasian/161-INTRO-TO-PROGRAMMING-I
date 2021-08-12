938e63a3f29451619c2d8122d37a4c612aeffa72
tokenasian <78766033+tokenasian@users.noreply.github.com>
2021-08-12 11:06:38 -0400

    Update LangtonAnt.py

--- LangtonAnt.py
+++ LangtonAnt.py
@@ -1,54 +1,58 @@
-class Ant:
 
+# Author: Matthew Armstrong
+# Date: 08/12/2021
+# Description: Langton's Ant program.
+class Ant:
+    """represents the ant"""
     def __init__(self):
-        self._size_x = 13  # size of grid
-        self._size_y = 13  # size of grid
-        self._direction = 0
-        self._initial_grid = [[0] * self._size_x for i in range(self._size_y)]
+        """private ant classes"""
+        self._size_x = 0  # size of grid
+        self._size_y = 0  # size of grid
         self._direction = 0
         self._grid = []
         self._move = 0
         self._required_moves = 100
         self._ant_x = 5  # pos
         self._ant_y = 5  # pos
-        self._get_input_grid_size()
-        self._validate_grid_size()
-        self._get_ant_starting_pos()
-        self._validate_ant_starting_pos()
-        self._get_ant_orientation()
-        self._get_ant_steps()
-        self._move_ant()
 
     def _get_input_grid_size(self):
         """get the grid size"""
-        _grid_size = int(input("Please enter a number no larger than 100 for the size of the square board:  "))
+        _grid_size = int(input(
+            "Please enter a number no larger than 100 for the size of the square board:  "))
         self._size_x = _grid_size
         self._size_y = _grid_size
 
         self._grid_builder()
 
     def _validate_grid_size(self):
+        """validate the grid size"""
         if self._size_x + 1 > 100 or self._size_x < 0:
-            print("invalid grid size input, number must be positive and must be under 100")
+            print(
+                "invalid grid size input, number must be positive and must be under 100")
             self._get_input_grid_size()
 
     def _get_ant_starting_pos(self):
         """get the ant position"""
-        self._ant_x = int(input("Please enter a number as the starting row number: "))
-        self._ant_y = int(input("Please enter a number as the starting column number : "))
+        self._ant_x = int(
+            input("Please enter a number as the starting row number: "))
+        self._ant_y = int(
+            input("Please enter a number as the starting column number : "))
 
     def _validate_ant_starting_pos(self):
+        """validate the ant position"""
         if self._ant_x > self._size_x - 1 or self._ant_y > self._size_y - 1 or self._ant_x < 0 or self._ant_y < 0:
             print("Ant starting position was invalid")
             self._get_ant_starting_pos()
 
     def _get_ant_orientation(self):
         """get the ant orientation"""
-        self._direction = int(input("Please choose the ant’s starting orientation:  "))
+        self._direction = int(
+            input("Please choose the ant’s starting orientation:  "))
 
     def _get_ant_steps(self):
         """get the number of steps for the ant"""
-        self._required_moves = int(input("Please enter the number of steps for the simulation : "))
+        self._required_moves = int(
+            input("Please enter the number of steps for the simulation : "))
 
     def _grid_builder(self):
         """make the grid"""
@@ -101,24 +105,21 @@ class Ant:
 
     def _move_ant(self):
         """make the ant move"""
-        if self._required_moves > self._move:
-            self._check_and_update_direction()
-            if self._direction == 0:
+        if self._required_moves >= self._move:
+            if self._move != -1:
+                self._check_and_update_direction()
+            if self._direction == 3:
                 self._move_up()
-            elif self._direction == 1:
+            elif self._direction == 0:
                 self._move_right()
-            elif self._direction == 2:
+            elif self._direction == 1:
                 self._move_down()
-            elif self._direction == 3:
+            elif self._direction == 2:
                 self._move_left()
 
             # incrementing the move counter
             self._move = self._move + 1
             self._grid[self._ant_x][self._ant_y] = 1 if self._grid[self._ant_x][self._ant_y] == 0 else 0
-            if self._move % 10 == 0:
-                print()
-                self._render()
-                print()
             self._move_ant()
         else:
             self._render()
@@ -127,14 +128,12 @@ class Ant:
         """start of render function"""
         new_grid = list(map(list, self._grid))
         new_grid[self._ant_x][self._ant_y] = 8
-        for x in new_grid:
+        print("")
+        for x in reversed(new_grid):
             for y in x:
                 white = "_"
-                # white = "　"
                 black = "#"
-                # black = "○"
                 cur_pos = "8"
-                # cur_pos = "●"
                 if y == 0 and y != 8:
                     position = white
                 elif y == 8:
@@ -144,9 +143,16 @@ class Ant:
                 print(position, end="")
             print(end="\n")
 
-    def run_stimulation(self):
-        """run the ant stimulation"""
+    def main(self):
+        """run ant simulation"""
+        self._get_input_grid_size()
+        self._validate_grid_size()
+        self._get_ant_starting_pos()
+        self._validate_ant_starting_pos()
+        self._get_ant_orientation()
+        self._get_ant_steps()
         self._move_ant()
 
 
 ant = Ant()
+ant.main()
