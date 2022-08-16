2693c02e195063af795d2344b248a3a5fb7363f0
tokenasian <78766033+tokenasian@users.noreply.github.com>
2022-08-16 11:47:56 -0400

    Create tempconvert.py

---
+++ Project 2/tempconvert.py
@@ -0,0 +1,8 @@
+# Author: Matthew Armstrong
+# Date: 6/22/21
+# Description: Writes a program that converts Celsius temperatures to Fahrenheit temperatures.
+print("Please enter a Celsius Temperature:")
+CelsiusTemp = float(input())
+FahrenheitTemp = (9/5) * CelsiusTemp + 32
+print("The equivalent Fahrenheit temperature is:")
+print(FahrenheitTemp)
