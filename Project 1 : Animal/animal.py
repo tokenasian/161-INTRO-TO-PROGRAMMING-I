6c22aab7a204ee373b7e8419e414841d60f838c4
tokenasian <78766033+tokenasian@users.noreply.github.com>
2022-08-16 11:45:43 -0400

    Delete Project 1 : Animal directory

--- Project 1 : Animal/animal.py
+++
@@ -1,7 +0,0 @@
-# Author: Matthew Armstrong
-# Date: 6/18/21
-# Description: Asks the user for their favorite animal and then
-#              prints out that that is their favorite animal
-
-fave_animal = input("Please enter your favorite animal: ")
-print("Your favorite animal is the " + fave_animal + ".")
