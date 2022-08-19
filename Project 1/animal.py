c53b0c2d585b4d42da3dfc04294f6d37493f0403
tokenasian <78766033+tokenasian@users.noreply.github.com>
2022-08-19 07:02:42 -0400

    Create animal.py

---
+++ Project 1/animal.py
@@ -0,0 +1,7 @@
+# Author: Matthew Armstrong
+# Date: 6/18/21
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
